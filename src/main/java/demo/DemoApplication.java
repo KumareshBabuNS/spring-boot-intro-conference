3d4e0c70f51104b0a27a538a50b4c64a5db3a9ea
Stephane Nicoll <snicoll@pivotal.io>
2017-05-16 14:55:58 +0300

    Spring Security demonstration
    If we had the `spring-boot-starter-security` starter, our app is secured
    using basic auth. This simple behaviour gets you starter with a default
    behaviour and invite you to look at the documentation for more options.
    
    The default creates a user name `user` with an auto-generated `password`
    that is printed on startup. That's rather annoying and we'd like to use
    a defined set of users. One way to do that is to add our own
    `GlobalAuthenticationConfigurerAdapter` config that defines an
    `AuthenticationManager` with two users: user has the `USER` role and
    hero has the `USER` and `HERO` roles.

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,12 +2,26 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Configuration
+	static class SecurityConfig extends GlobalAuthenticationConfigurerAdapter {
+
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication()
+					.withUser("hero").password("hero").roles("HERO", "USER").and()
+					.withUser("user").password("user").roles("USER");
+		}
 	}
 
 }
