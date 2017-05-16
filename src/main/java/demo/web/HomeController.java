a12f2d8edc093d27a5b2833d67954bbef3da5f43
Stephane Nicoll <snicoll@pivotal.io>
2017-05-16 14:43:38 +0300

    Simple controller demonstration
    This commit demonstrates how Spring Boot takes care of the default
    configuration for a web application. If you add `spring-boot-starter-web`
    to your project, an embedded container (Tomcat) and Spring MVC will be
    automatically configured unless you state otherwise.
    
    Adding a controller on `/` makes your app reachable at
    `http://localhost:8080` by default.

---
+++ src/main/java/demo/web/HomeController.java
@@ -0,0 +1,14 @@
+package demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello World";
+	}
+
+}
