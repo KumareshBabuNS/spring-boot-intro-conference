bf935cd7115d4d7afe66b5cef68a8f870b8fd5ce
Stephane Nicoll <snicoll@pivotal.io>
2017-05-16 15:10:27 +0300

    Devtools demonstration
    Spring Boot provide development tools when you are working on your
    application in your IDE. If you add `spring-boot-devtools`, we'll
    automatically tune your app for local development:
    
    * All template engine caches will be disabled
    * If H2 is present the h2 console will be exposed at `/h2-console`
    * A LiveReload server will be started as part of the app, allowing you
    to connect to your app using the free LiveReload browser plugin
    
    But most importantly, Spring Boot will watch your files and every time
    the classpath changes it will either refresh the browser (via
    LiveReload) or restart the application and refresh the browser when it's
    done.
    
    Concretely, if you are working on a template engine or a static resource
    and that one changes, the browser will refresh automatically. IF you
    change anything else (code, configuration, etc), the application will
    restart to apply those changes.
    
    If you try to create an error page in `static/error/404.html`, you'll
    notice any unmapped URI will redirect to that page. We could also add
    a custom health indicator and show that `/health` shows it once we've
    build the project.

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,7 +1,12 @@
 package demo;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
@@ -22,6 +27,18 @@ public class DemoApplication {
 					.withUser("hero").password("hero").roles("HERO", "USER").and()
 					.withUser("user").password("user").roles("USER");
 		}
+	}
+
+	@Bean
+	public HealthIndicator myHealthIndicator() {
+		return () -> {
+			if (new Random().nextBoolean()) {
+				return Health.up().build();
+			}
+			else {
+				return Health.down().withDetail("booooooo", 42).build();
+			}
+		};
 	}
 
 }
